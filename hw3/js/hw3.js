f44410c6f6b8ed0c69e535d05b735c6d187f0ee3
andrey <gusakov.andrei@gmail.com>
2020-11-17 04:34:58 +0300

    hw3++

--- hw3/js/hw3.js
+++ hw3/js/hw3.js
@@ -18,24 +18,44 @@ while (e >= 0) {
 
 // Задача 3
 
-// let plate = 7, washer = 2.5;
-//     while (plate > 0) {
-//         if (washer > 0) {
-//             plate -= 1;
-//             washer -= 0.5;
-//             console.log(`Осталось моющего средства: ${washer}`);
-//             console.log(`Осталось тарелок: ${plate}`);
-//         }
-//     }
+let plate = 7, washer = 2.5; // как в while объявить два условия? почему-то мой код этого задания подвешиванит страницу, она не прогружается
+    while (plate > 0) {
+        if (washer > 0) {
+            plate -= 1;
+            washer -= 0.5;
+            console.log(`Осталось моющего средства: ${washer}`);
+            console.log(`Осталось тарелок: ${plate}`);
+        }
+    }
 
 
 // Задача массив 1
 
+let array = [];
+array.length = 10;
+// console.log(array);
+for (let index = 0; index < array.length; index++) {
+    array[index] = Math.floor(Math.random()*100);
+}
+console.log(array)
+console.log(array[Math.floor(Math.random()*100)]) // вывод рандом значения
+let summ = 0; 
+    for (let element of array) { 
+        summ += element;
+}
+console.log(summ);
+let mid = summ / array.length; // среднее зачение
+console.log(mid);
+
+
 // Задача массив 2
+
+let arr = [4, 9, 1, 3, 5, 2, 8]; // не понял
+
 
 // Задача массив 3
 
-let nums = [
+let nums = [    // не понял, как сложить многомерный массив, выводится просто в сроку при моём решении
     [23, 56, 78, 12, -900],
     [0, 0, 67, -3, 621],
     [89, 67, 12],
